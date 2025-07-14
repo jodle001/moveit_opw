e33153f7252706fe2428046c6d04ef1acd709a82
jodle001 <60989139+jodle001@users.noreply.github.com>
2025-07-14 11:22:00 -0400

    Update moveit_opw_kinematics_plugin.h

--- moveit_opw_kinematics_plugin/include/moveit_opw_kinematics_plugin/moveit_opw_kinematics_plugin.h
+++ moveit_opw_kinematics_plugin/include/moveit_opw_kinematics_plugin/moveit_opw_kinematics_plugin.h
@@ -15,9 +15,9 @@
 #include <moveit_msgs/msg/move_it_error_codes.hpp>
 
 // MoveIt!
-#include <moveit/kinematics_base/kinematics_base.hpp>
-#include <moveit/robot_model/robot_model.hpp>
-#include <moveit/robot_state/robot_state.hpp>
+#include <moveit/kinematics_base/kinematics_base.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
 
 // OPW kinematics
 #include "opw_kinematics/opw_kinematics.h"
